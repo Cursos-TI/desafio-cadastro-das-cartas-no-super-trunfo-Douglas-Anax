578836d25945cb940db2a603b4cb56beab007864
Douglas-Anax <48124155+Douglas-Anax@users.noreply.github.com>
2025-01-25 13:45:14 +0000

    Feito cadastro de cidades

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,204 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+    char A01nome[50];
+    float A01populacao;
+    float A01area;
+    float A01pib;
+    int A01turisticos;
+
+    char A02nome[50];
+    float A02populacao;
+    float A02area;
+    float A02pib;
+    int A02turisticos;
+
+    char A03nome[50];
+    float A03populacao;
+    float A03area;
+    float A03pib;
+    int A03turisticos;
+
+    char A04nome[50];
+    float A04populacao;
+    float A04area;
+    float A04pib;
+    int A04turisticos;
+
+    char B01nome[50];
+    float B01populacao;
+    float B01area;
+    float B01pib;
+    int B01turisticos;
+
+    char B02nome[50];
+    float B02populacao;
+    float B02area;
+    float B02pib;
+    int B02turisticos;
+
+    char B03nome[50];
+    float B03populacao;
+    float B03area;
+    float B03pib;
+    int B03turisticos;
+
+    char B04nome[50];
+    float B04populacao;
+    float B04area;
+    float B04pib;
+    int B04turisticos;
+
+    char C01nome[50];
+    float C01populacao;
+    float C01area;
+    float C01pib;
+    int C01turisticos;
+
+    char C02nome[50];
+    float C02populacao;
+    float C02area;
+    float C02pib;
+    int C02turisticos;
+
+    char C03nome[50];
+    float C03populacao;
+    float C03area;
+    float C03pib;
+    int C03turisticos;
+
+    char C04nome[50];
+    float C04populacao;
+    float C04area;
+    float C04pib;
+    int C04turisticos;
+
+    char D01nome[50];
+    float D01populacao;
+    float D01area;
+    float D01pib;
+    int D01turisticos;
+
+    char D02nome[50];
+    float D02populacao;
+    float D02area;
+    float D02pib;
+    int D02turisticos;
+
+    char D03nome[50];
+    float D03populacao;
+    float D03area;
+    float D03pib;
+    int D03turisticos;
+
+    char D04nome[50];
+    float D04populacao;
+    float D04area;
+    float D04pib;
+    int D04turisticos;
+
+    char E01nome[50];
+    float E01populacao;
+    float E01area;
+    float E01pib;
+    int E01turisticos;
+
+    char E02nome[50];
+    float E02populacao;
+    float E02area;
+    float E02pib;
+    int E02turisticos;
+
+    char E03nome[50];
+    float E03populacao;
+    float E03area;
+    float E03pib;
+    int E03turisticos;
+
+    char E04nome[50];
+    float E04populacao;
+    float E04area;
+    float E04pib;
+    int E04turisticos;
+
+    char F01nome[50];
+    float F01populacao;
+    float F01area;
+    float F01pib;
+    int F01turisticos;
+
+    char F02nome[50];
+    float F02populacao;
+    float F02area;
+    float F02pib;
+    int F02turisticos;
+
+    char F03nome[50];
+    float F03populacao;
+    float F03area;
+    float F03pib;
+    int F03turisticos;
+
+    char F04nome[50];
+    float F04populacao;
+    float F04area;
+    float F04pib;
+    int F04turisticos;
+
+    char G01nome[50];
+    float G01populacao;
+    float G01area;
+    float G01pib;
+    int G01turisticos;
+
+    char G02nome[50];
+    float G02populacao;
+    float G02area;
+    float G02pib;
+    int G02turisticos;
+
+    char G03nome[50];
+    float G03populacao;
+    float G03area;
+    float G03pib;
+    int G03turisticos;
+
+    char G04nome[50];
+    float G04populacao;
+    float G04area;
+    float G04pib;
+    int G04turisticos;
+
+    char H01nome[50];
+    float H01populacao;
+    float H01area;
+    float H01pib;
+    int H01turisticos;
+
+    char H02nome[50];
+    float H02populacao;
+    float H02area;
+    float H02pib;
+    int H02turisticos;
+
+    char H03nome[50];
+    float H03populacao;
+    float H03area;
+    float H03pib;
+    int H03turisticos;
+
+    char H04nome[50];
+    float H04populacao;
+    float H04area;
+    float H04pib;
+    int H04turisticos;
+
+    printf("Bem Vindo ao SuperTrunfo! \nVocê cadastrará 4 cidades deste estado. Cada cidade com 5 informações.\n");
+    printf("Vamos começar? Cadastre os dados da primeira cidade: \n");
+    printf("Nome: \n População: \n Área: \n PIB: \n Pontos Turisticos: \n");
+    scanf("%s \n, %f \n, %f \n, %f \n, %d \n", &A01nome[50], &A01populacao, &A01area, &A01pib, &A01turisticos);
 
     return 0;
 }
